84c84de5480ebb97ff66aeff964b286044367f15
Marlon Lopez <marlonlop.cs@gmail.com>
2023-09-28 02:06:36 -0600

    leetcode_199

---
+++ trees/199-Binary_tree_rigth_side_view.py
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        if not root:
+            return None
+        
+        res = []
+        q = deque([root])
+        while q:
+            len_q = len(q)
+            for i in range(len_q):
+                node = q.popleft()
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+                if i == len_q - 1:
+                    res.append(node.val)
+                        
+        return res

ad3069f5fe18becf2719980e75a5076c3a020cb3
Marlon Lopez <marlonlop.cs@gmail.com>
2023-09-30 20:14:49 -0600

    leetcode_78

---
+++ backtracking/78-Subsets.py
@@ -0,0 +1,28 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        powerSet = []
+        subset = []
+
+        def backtrack(idx):
+            if idx >= len(nums):
+                powerSet.append(subset.copy())
+                return
+            
+            # decision tree to include current nums[idx]
+            subset.append(nums[idx])
+            backtrack(idx + 1)
+
+            # decision tree NOT to include current nums[idx]
+            subset.pop() # removing current nums[idx] that was added in line 12
+            backtrack(idx + 1)
+        
+        backtrack(0)
+        return powerSet
+
+'''
+time O(n * 2^n)
+space O(n)                                  *
+                            [1]                               []
+                [1,2]               [1]               [2]             []
+         [1,2,3]     [1,2]     [1,3]    [1]      [2,3]   [2]      [3]    []  
+'''

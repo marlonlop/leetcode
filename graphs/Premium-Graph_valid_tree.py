ec713346ec4de6f3f358dda2f407b4cf73f87714
Marlon Lopez <marlonlop.cs@gmail.com>
2023-10-01 22:38:57 -0600

    leetcode_premium_graph

---
+++ graphs/Premium-Graph_valid_tree.py
@@ -0,0 +1,30 @@
+class Solution:
+    """
+    @param n: An integer
+    @param edges: a list of undirected edges
+    @return: true if it's a valid tree, or false
+    """
+    def valid_tree(self, n: int, edges: List[List[int]]) -> bool:
+        # write your code here
+        if not n:
+            return True
+        
+        adjacency = {i:[] for i in range(n)} 
+        for e1, e2 in edges:
+            adjacency[e1].append(e2)
+            adjacency[e2].append(e1)
+        
+        visited = ()
+        def dfs(vertex, prev):
+            if vertex in visited:
+                return False
+
+            visited.add(vertex)
+            for e in adjacency[vertex]:
+                if e == prev: # ignoring edge that loops back to parent bc given undirected graph
+                    continue
+                if not dfs(e, vertex): #detected lood from dfs base case
+                    return False 
+            return True #bc no loop detected
+        
+        return dfs(0, -1) and len(visited) == n # make sure graph is tree

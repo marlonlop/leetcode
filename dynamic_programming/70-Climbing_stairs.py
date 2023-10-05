f683991d8e9c0c55f6457402268eada558e7fe01
Marlon Lopez <marlonlop.cs@gmail.com>
2023-10-04 23:38:23 -0600

    leetcode_premium_70

--- dynamic_programming/70-Climbing_stairs.py
+++ dynamic_programming/70-Climbing_stairs.py
@@ -13,3 +13,13 @@ class Solution:
             s1 = s2
             s2 = current
         return s2
+
+        '''
+        # bottom up dp solution
+        s1, s2 = 1, 1
+        for i in range(n - 1):
+            temp = s1
+            s1 = s1 + s2
+            s2 = temp
+        return s1
+        '''

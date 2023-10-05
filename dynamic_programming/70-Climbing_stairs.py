2fb6f5dfa8845146c98e1f0a4b74354fb2fb04ba
Marlon Lopez <marlonlop.cs@gmail.com>
2023-10-04 23:26:46 -0600

    leetcode_premium_70

---
+++ dynamic_programming/70-Climbing_stairs.py
@@ -0,0 +1,15 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        '''
+        stairs    1     2    3        4     5
+        # ways    1     2  2+1=3    2+3=5  3+5=8   #same as fibonacci sequence
+        '''
+        # base case
+        if n <= 2:#
+            return n 
+        s1, s2 = 1, 1 #for memoization
+        for n in range(1, n):
+            current = s1 + s2 #temp to keep sum of s1, s2 before rewriting them
+            s1 = s2
+            s2 = current
+        return s2
